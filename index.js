a64c35b853255b6e67eec43b9fdc3be3e1515950
Conner Hasbrouck <connerhasbrouck@gmail.com>
2018-11-29 10:31:18 -0600

    web socket hookup and more styling

---
+++ index.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path')
+
+app.use(express.static(__dirname + '/public'));
+
+//status area object
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+
+  //on user connect
+  io.emit('chat message', 'VR-BOX UI is connected');
+  console.log('VR-BOX UI is connected');
+
+  //on button press
+  socket.on('button press', function(id) {
+    //whatever functions we need to run for VR-BOX or VR Experience
+    console.log(id);
+
+    //status update
+    io.emit('status update', id);
+  });
+
+});
+
+io.emit('some event', { for: 'everyone' });
+
+http.listen(3000, function() {
+  console.log('listening on *:3000');
+});
